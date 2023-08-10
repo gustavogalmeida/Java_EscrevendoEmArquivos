c0f5322932da9001a4e1da4e484f6690a2ecc82a
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2023-08-10 19:45:09 -0300

    Codigo finalizado e funcionando

--- src/com/escrita/Main.java
+++ src/com/escrita/Main.java
@@ -1,7 +1,33 @@
 package com.escrita;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Olá, mundo!");
+	public static void main(String[] args) throws IOException{
+		
+		ArrayList<String> linhas = new ArrayList<>();
+		
+		Scanner scanner = new Scanner(System.in);
+		String entrada = new String();
+		
+		System.out.println("DIgite suas tarefas abaixo, "
+				+ "cada tarefa separada por um Enter");
+		
+		do {
+			entrada = scanner.nextLine();
+			linhas.add(entrada);
+		} while (entrada!=""); 
+		
+		Path path = Paths.get("/home/gustavopc/Desktop/tarefas.txt");
+		
+		Files.write(path, linhas);
+		
+		System.out.println("Fim...");
 	}
 }
